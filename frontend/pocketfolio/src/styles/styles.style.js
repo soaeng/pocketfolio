d9fbdfc5973e8082152e1d9485f417c235d7ce68
강민재 <kmj5052@gmail.com>
2022-10-25 14:03:09 +0900

    [Add] 반응형 폰트 Body3, Body4 추가

--- frontend/pocketfolio/src/styles/styles.style.js
+++ frontend/pocketfolio/src/styles/styles.style.js
@@ -124,4 +124,54 @@ const Body2 = styled.p`
     font-size: 0.5rem;
   }
 `;
-export {H1, H2, H3, Body1, Body2};
+
+const Body3 = styled.p`
+  // xl(4k, ≥2100px)
+  font-size: 1.5rem;
+
+  // lg(FHD <2100px)
+  @media screen and (max-width: 2100px) {
+    font-size: 1rem;
+  }
+
+  // md(HD, <1500px)
+  @media screen and (max-width: 1500px) {
+    font-size: 0.75rem;
+  }
+
+  // sm(태블릿, <900px)
+  @media screen and (max-width: 900px) {
+    font-size: 0.5rem;
+  }
+
+  // xs(모바일, <600px)
+  @media screen and (max-width: 600px) {
+    font-size: 0.35rem;
+  }
+`;
+
+const Body4 = styled.p`
+  // xl(4k, ≥2100px)
+  font-size: 1rem;
+
+  // lg(FHD <2100px)
+  @media screen and (max-width: 2100px) {
+    font-size: 0.75rem;
+  }
+
+  // md(HD, <1500px)
+  @media screen and (max-width: 1500px) {
+    font-size: 0.5rem;
+  }
+
+  // sm(태블릿, <900px)
+  @media screen and (max-width: 900px) {
+    font-size: 0.35rem;
+  }
+
+  // xs(모바일, <600px)
+  @media screen and (max-width: 600px) {
+    font-size: 0.25rem;
+  }
+`;
+export {H1, H2, H3, Body1, Body2, Body3, Body4};
