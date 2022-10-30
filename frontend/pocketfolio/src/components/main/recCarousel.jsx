70bd380dbad968f4d2fcb6469a9c0aec64ab9c10
안정현 <kkk0125-@naver.com>
2022-10-30 23:08:16 +0900

    [ADD] search component, carousel add data

--- frontend/pocketfolio/src/components/main/recCarousel.jsx
+++ frontend/pocketfolio/src/components/main/recCarousel.jsx
@@ -1,30 +1,51 @@
 // 추천 carousel
 import React, { useState, useEffect, useRef } from "react";
-import {Container, Carousel, Item, Ui} from './recCarousel.style'
+import {Container, Carousel, Item, Ui, Item3} from './recCarousel.style'
 
 // 임시데이터
 const items = [
     {
         icon:"face",
-        copy:'01. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        copy:'01. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        name: '김싸피',
+        like: '123',
+        seen: '234'
     },{
         icon:"pets",
-        copy:'02. Sed do eiusmod tempor incididunt ut labore.'
+        copy:'02. Sed do eiusmod tempor incididunt ut labore.',
+        name: '김싸피',
+        like: '123',
+        seen: '234'
     },{
         icon:"stars",
-        copy:'03. Consectetur adipiscing elit.'
+        copy:'03. Consectetur adipiscing elit.',
+        name: '김싸피',
+        like: '123',
+        seen: '234'
     },{
         icon:"invert_colors",
-        copy:'04. Ut enim ad minim veniam, quis nostrud exercitation.'
+        copy:'04. Ut enim ad minim veniam, quis nostrud exercitation.',
+        name: '김싸피',
+        like: '123',
+        seen: '234'
     },{
         icon:"psychology",
-        copy:'05. Llamco nisi ut aliquip ex ea commodo consequat.'
+        copy:'05. Llamco nisi ut aliquip ex ea commodo consequat.',
+        name: '김싸피',
+        like: '123',
+        seen: '234'
     },{
         icon:"brightness_7",
-        copy:'06. Misi ut aliquip ex ea commodo consequat.'
+        copy:'06. Misi ut aliquip ex ea commodo consequat.',
+        name: '김싸피',
+        like: '123',
+        seen: '234'
     },{
       icon:"brightness",
-      copy:'07. Misi ut aliquip ex ea commodo consequat.'
+      copy:'07. Misi ut aliquip ex ea commodo consequat.',
+      name: '김싸피',
+      like: '123',
+      seen: '234'
   }
 ];
 
@@ -44,24 +65,30 @@ const RecCarousel = () => {
 
   return(
     <>
+      <p>포켓폴리오 추천 작품</p>
     <Container>
       <Carousel ref={carousel}>
         {item.map((it) => {
-          const {icon, copy} = it;
+          const {icon, copy, name, like, seen} = it;
           return(
             <Item>
               <div>{icon}</div>
               <div>{copy}</div>
+              <hr/>
+              <Item3>
+                <div>{name}</div>
+                <div>❤ {like}</div>
+                <div>👁 {seen}</div>
+              </Item3>
             </Item>
           )
         })}
       </Carousel>
-      
     </Container>
     <Ui>
-    <button onClick={handleLeft}>{"<"}</button>
-    <button onClick={handleRight}>{">"}</button>
-  </Ui>
+      <button onClick={handleLeft}>{"<"}</button>
+      <button onClick={handleRight}>{">"}</button>
+    </Ui>
   </>
   );
 }
--- frontend/pocketfolio/src/components/main/recCarousel.style.js
+++ frontend/pocketfolio/src/components/main/recCarousel.style.js
@@ -16,6 +16,9 @@ export const Carousel = styled.div`
 `
 
 export const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   margin: 10px;
   padding: 10px;
   width: 305px;
@@ -26,7 +29,7 @@ export const Item = styled.div`
 
 export const Ui = styled.div`
   position: absolute;
-    top: 50%;
+    top: 60%;
     transform: translateY(-50%);
     display: flex;
     width: calc(100% - 12px * 2);
@@ -57,4 +60,9 @@ export const Ui = styled.div`
         border: 1px solid rgba(255, 255, 255, 1);
       }
   }
+`
+
+export const Item3 = styled.div`
+  display:flex;
+  justify-content: space-between;
 `
