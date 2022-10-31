f4e3b329ffadcb4338723d92a42068a7275bca0f
이승현 <window8397@naver.com>
2022-10-31 13:33:19 +0900

    [Add] three.js library

--- frontend/pocketfolio/src/components/room/Room.jsx
+++ frontend/pocketfolio/src/components/room/Room.jsx
@@ -1,12 +1,32 @@
-import RoomNav from "./RoomNav";
-
+import RoomNav from './RoomNav';
+import {Container, ThreeCanvas} from './Room.style';
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls} from '@react-three/drei';
+import {useParams} from 'react-router-dom';
 
 const Room = () => {
+  const params = useParams();
+  const room_id = parseInt(params.room_id);
+
   return (
-    <>
+    <Container>
       <RoomNav />
-    </>
-  ); 
+
+      <Canvas>
+        <OrbitControls autoRotate={false} />
+        <gridHelper />
+        <axesHelper />
+
+        <mesh>
+          <ambientLight intensity={1} />
+          <directionalLight position={[10, 10, 10]} intensity={1} />
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" color={0xa3b18a} />
+        </mesh>
+
+      </Canvas>
+    </Container>
+  );
 };
 
 export default Room;
--- frontend/pocketfolio/src/components/room/RoomNav.jsx
+++ frontend/pocketfolio/src/components/room/RoomNav.jsx
@@ -1,5 +1,49 @@
-const RoomNav = () => {
-  return <>RoomNav</>;
-};
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {
+  Container,
+  LogoContainer,
+  LogoImg,
+  NavBotton,
+  LoginDiv,
+} from './RoomNav.style';
+import Avatar from '../common/avatar';
 
+const RoomNav = () => {
+  const navigate = useNavigate();
+
+  // 로그인 표시 => 수정 필요
+  const [loginFlag, setLoginFlag] = useState(() =>
+    sessionStorage.getItem('Id'),
+  );
+
+  const logoClickHandler = () => {
+    navigate('/main');
+  };
+
+  const loginClickHandler = () => {
+    navigate('/login');
+  };
+
+  const roomClickHandler = () => {
+    navigate('/port');
+  };
+
+  return (
+    <Container>
+      <LogoContainer onClick={logoClickHandler}>
+        <LogoImg src={process.env.PUBLIC_URL + '/assets/images/logo2.png'} />
+      </LogoContainer>
+
+      {loginFlag === null ? (
+        <NavBotton onClick={loginClickHandler}>로그인/회원가입</NavBotton>
+      ) : (
+        <LoginDiv>
+          <NavBotton onClick={roomClickHandler}>마이룸</NavBotton>
+          <Avatar />
+        </LoginDiv>
+      )}
+    </Container>
+  );
+};
 export default RoomNav;
