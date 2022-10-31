8b2b64851320a5480a9dfb1055c5e534db998dce
이승현 <window8397@naver.com>
2022-10-31 11:31:13 +0900

    [Add] Route & edit Route

--- frontend/pocketfolio/src/App.js
+++ frontend/pocketfolio/src/App.js
@@ -1,27 +1,44 @@
-import logo from './logo.svg';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Landing from './components/landing/Landing';
 import Main from './components/main/Main';
-import Portfolio from './components/portfolio/Portfolio';
-import Room from './components/room/Room';
 import Search from './components/search/Search';
+
+// Room
+import Room from './components/room/Room';
+import RoomEdit from './components/room/RoomEdit';
+
+// User
 import Login from './components/user/Login';
 import Profile from './components/user/Profile';
+
+// Portfolio
+import AddPort from './components/portfolio/AddPort';
+import Portfolio from './components/portfolio/Portfolio';
+import PortDetail from './components/portfolio/PortDetail';
 import PortfolioEdit from './components/portfolio/PortfolioEdit';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/main" element={<Main/>}/>
-        <Route path="/port" element={<Portfolio/>}/>
-        <Route path="/port-edit" element={<PortfolioEdit/>}/>
-        <Route path="/room" element={<Room/>}/>
-        <Route path="/search" element={<Search/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/search" element={<Search />} />
+
+        {/* Room */}
+        <Route path="/room/:room_id" element={<Room />} />
+        <Route path="/room/edit/:room_id" element={<RoomEdit />} />
+
+        {/* User */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+
+        {/* Portfolio */}
+        <Route path="/port" element={<Portfolio />} />
+        <Route path="/port/create" element={<AddPort />} />
+        <Route path="/port/:port_id" element={<PortDetail />} />
+        <Route path="/port/edit/:port_id" element={<PortfolioEdit />} />
       </Routes>
     </BrowserRouter>
   );
