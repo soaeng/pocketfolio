782188006f3695349b10fdd99d7a94cb04ee7501
이소영 <sodud0695@naver.com>
2022-11-08 11:17:44 +0900

    [Add] add swagger

--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/controller/PortfolioController.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/controller/PortfolioController.java
@@ -2,7 +2,12 @@ package com.ssafy.pocketfolio.api.controller;
 
 import com.ssafy.pocketfolio.api.dto.request.PortfolioReq;
 import com.ssafy.pocketfolio.api.dto.response.PortfolioRes;
+import com.ssafy.pocketfolio.api.dto.response.UserRes;
 import com.ssafy.pocketfolio.api.service.PortfolioServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +28,11 @@ public class PortfolioController {
 
     private final PortfolioServiceImpl portfolioService;
 
+    @Operation(summary = "포트폴리오 등록", description = "포트폴리오 등록 시 포트폴리오 정보는 application/json 형식, thumbnail은 이미지 파일, files은 첨부파일(다중 첨부 가능)", responses = {
+            @ApiResponse(responseCode = "201", description = "포트폴리오 등록 성공", content = @Content(schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PostMapping
     private ResponseEntity<Long> insertPortfolio(HttpServletRequest request, @RequestPart(value="portfolio") PortfolioReq portfolio, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail, @RequestPart(value = "files", required = false) List<MultipartFile> files) {
         log.debug("[POST] Controller - Portfolio");
@@ -45,7 +55,11 @@ public class PortfolioController {
         return new ResponseEntity<>(response, status);
     }
 
-    // 본인의 포트폴리오 목록
+    @Operation(summary = "내 포트폴리오 목록", description = "내 포트폴리오 목록 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "포트폴리오 목록 조회 성공", content = @Content(schema = @Schema(implementation = PortfolioRes.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping
     private ResponseEntity<List<PortfolioRes>> findPortfolioList(HttpServletRequest request){
         log.debug("[GET] Controller - findPortfolioList");
@@ -68,6 +82,11 @@ public class PortfolioController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "포트폴리오 상세 조회", description = "포트폴리오 상세 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "포트폴리오 목록 조회 성공", content = @Content(schema = @Schema(implementation = PortfolioRes.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/{portSeq}")
     private ResponseEntity<PortfolioRes> findPortfolio(@PathVariable(value = "portSeq") long portSeq){
         log.debug("[GET] Controller - findPortfolio");
@@ -82,7 +101,12 @@ public class PortfolioController {
         }
         return new ResponseEntity<>(response, status);
     }
-
+    
+    @Operation(summary = "포트폴리오 수정", description = "포트폴리오 수정", responses = {
+            @ApiResponse(responseCode = "201", description = "포트폴리오 수정 성공 후 수정된 포트폴리오 번호 반환", content = @Content(schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PatchMapping("/{portSeq}")
     private ResponseEntity<Long> updatePortfolio(@PathVariable(value="portSeq") long portSeq, @RequestPart(value="portfolio") PortfolioReq portfolio, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail, @RequestPart(value = "files", required = false) List<MultipartFile> files, HttpServletRequest request) {
         log.debug("[PATCH] Controller - Portfolio");
@@ -107,6 +131,11 @@ public class PortfolioController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "포트폴리오 삭제", description = "포트폴리오 삭제", responses = {
+            @ApiResponse(responseCode = "200", description = "포트폴리오 삭제 성공", content = @Content(schema = @Schema(implementation = Boolean.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @DeleteMapping("/{portSeq}")
     private ResponseEntity<Boolean> deletePortfolio(@PathVariable(value="portSeq") long portSeq, HttpServletRequest request) {
         log.debug("[DELETE] Controller - Portfolio");
--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/controller/RoomController.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/controller/RoomController.java
@@ -2,7 +2,13 @@ package com.ssafy.pocketfolio.api.controller;
 
 import com.ssafy.pocketfolio.api.dto.request.RoomReq;
 import com.ssafy.pocketfolio.api.dto.response.RoomDetailRes;
+import com.ssafy.pocketfolio.api.dto.response.UserRes;
 import com.ssafy.pocketfolio.api.service.RoomServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,10 +23,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/rooms")
 @RequiredArgsConstructor
+@Tag(name = "RoomController", description = "마이룸 API")
 public class RoomController {
 
     private final RoomServiceImpl roomService;
 
+    @Operation(summary = "마이룸 등록", description = "마이룸 등록", responses = {
+            @ApiResponse(responseCode = "201", description = "마이룸 등록 성공", content = @Content(schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PostMapping
     public ResponseEntity<Long> insertRoom(@RequestPart(value = "room") RoomReq roomReq, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail, HttpServletRequest request){
         log.debug("[POST] Controller - insertRoom");
@@ -45,6 +57,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 목록 조회", description = "마이룸 목록 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "마이룸 목록 조회 성공", content = @Content(schema = @Schema(implementation = RoomDetailRes.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping
     public ResponseEntity<List<RoomDetailRes>> findRoomList(HttpServletRequest request) {
         log.debug("[GET] Controller - findRoomList");
@@ -62,6 +79,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 조회", description = "마이룸 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "마이룸 조회 성공", content = @Content(schema = @Schema(implementation = RoomDetailRes.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/{roomSeq}")
     public ResponseEntity<RoomDetailRes> findRoom(@PathVariable(value = "roomSeq") Long roomSeq, HttpServletRequest request){
         log.debug("[GET] Controller - findRoom");
@@ -85,6 +107,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 수정", description = "마이룸 수정", responses = {
+            @ApiResponse(responseCode = "201", description = "마이룸 수정 성공", content = @Content(schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PatchMapping("/{roomSeq}")
     public ResponseEntity<Long> updateRoom(@PathVariable(value = "roomSeq") Long roomSeq, @RequestPart(value = "room") RoomReq roomReq, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail, HttpServletRequest request){
         log.debug("[PATCH] Controller - updateRoom");
@@ -109,6 +136,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 삭제", description = "마이룸 삭제", responses = {
+            @ApiResponse(responseCode = "200", description = "마이룸 삭제 성공", content = @Content(schema = @Schema(implementation = Boolean.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @DeleteMapping("/{roomSeq}")
     public ResponseEntity<Boolean> deleteRoom(@PathVariable(value = "roomSeq") Long roomSeq, HttpServletRequest request){
         log.debug("[DELETE] Controller - deleteRoom");
@@ -131,6 +163,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 좋아요", description = "마이룸 좋아요", responses = {
+            @ApiResponse(responseCode = "201", description = "마이룸 좋아요 완료", content = @Content(schema = @Schema(implementation = Boolean.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PostMapping("/like/{roomSeq}")
     public ResponseEntity<Boolean> insertRoomLike(@PathVariable(value = "roomSeq") Long roomSeq, HttpServletRequest request){
         log.debug("[POST] Controller - insertRoomLike");
@@ -153,6 +190,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 좋아요 취소", description = "마이룸 좋아요 취소", responses = {
+            @ApiResponse(responseCode = "200", description = "마이룸 좋아요 취소 완료", content = @Content(schema = @Schema(implementation = Boolean.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @DeleteMapping("/like/{roomSeq}")
     public ResponseEntity<Boolean> deleteRoomLike(@PathVariable(value = "roomSeq") Long roomSeq, HttpServletRequest request){
         log.debug("[DELETE] Controller - deleteRoomLike");
@@ -175,6 +217,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 좋아요 목록", description = "마이룸 좋아요 목록", responses = {
+            @ApiResponse(responseCode = "200", description = "마이룸 좋아요 목록 조회 완료", content = @Content(schema = @Schema(implementation = RoomDetailRes.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/like")
     public ResponseEntity<List<RoomDetailRes>> findRoomLikeList(HttpServletRequest request) {
         List<RoomDetailRes> response = null;
@@ -195,6 +242,11 @@ public class RoomController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "마이룸 좋아요 순 목록 조회", description = "마이룸 좋아요 순 목록 조회", responses = {
+            @ApiResponse(responseCode = "201", description = "마이룸 좋아요 순 목록 조회 성공", content = @Content(schema = @Schema(implementation = RoomDetailRes.class))),
+            @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/best")
     public ResponseEntity<List<RoomDetailRes>> findRoomBestList(HttpServletRequest request) {
         List<RoomDetailRes> response = null;
