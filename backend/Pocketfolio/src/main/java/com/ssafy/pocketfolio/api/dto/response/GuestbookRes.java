f392ac196dbb0177453ee4c05f22c4923e659c81
이소영 <sodud0695@naver.com>
2022-11-09 11:45:13 +0900

    [Add] 방명록 등록

--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/dto/response/GuestbookRes.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/dto/response/GuestbookRes.java
@@ -1,15 +1,34 @@
 package com.ssafy.pocketfolio.api.dto.response;
 
+import com.ssafy.pocketfolio.db.entity.Guestbook;
 import com.ssafy.pocketfolio.db.entity.Room;
 import com.ssafy.pocketfolio.db.entity.User;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public class GuestbookRes {
     private Long guestbookSeq;
     private String content;
     private String isPublic;
     private LocalDateTime created;
-    private Room room;
-    private User user;
+    private Long roomSeq;
+    private Long userSeq;
+    private String userName;
+
+    public static GuestbookRes toDto(Guestbook entity){
+        if(entity == null){
+            return null;
+        }
+        return GuestbookRes.builder()
+                .guestbookSeq(entity.getGuestbookSeq())
+                .content(entity.getContent())
+                .isPublic(entity.getIsPublic())
+                .created(entity.getCreated())
+                .roomSeq(entity.getRoom().getRoomSeq())
+                .userSeq(entity.getUser().getUserSeq())
+                .userName(entity.getUser().getName())
+                .build();
+    }
 }
--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/service/GuestbookServiceImpl.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/service/GuestbookServiceImpl.java
@@ -3,43 +3,74 @@ package com.ssafy.pocketfolio.api.service;
 import com.ssafy.pocketfolio.api.dto.request.GuestbookCommentReq;
 import com.ssafy.pocketfolio.api.dto.request.GuestbookReq;
 import com.ssafy.pocketfolio.api.dto.response.GuestbookRes;
+import com.ssafy.pocketfolio.db.entity.Guestbook;
+import com.ssafy.pocketfolio.db.entity.User;
 import com.ssafy.pocketfolio.db.repository.GuestbookRepository;
+import com.ssafy.pocketfolio.db.repository.RoomRepository;
+import com.ssafy.pocketfolio.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService {
 
+    private final UserRepository userRepository;
     private final GuestbookRepository guestbookRepository;
 
     @Override
     public Long insertGuestbook(GuestbookReq req, long userSeq) {
-
-        return null;
+        log.debug("[POST] Service - insertGuestbook");
+        User user = userRepository.findById(userSeq).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
+        try {
+            Guestbook guestbook = GuestbookReq.toEntity(req, user);
+            Long guestbookSeq = guestbookRepository.save(guestbook).getGuestbookSeq();
+            log.debug("저장된 방명록 번호: " + guestbookSeq);
+            return guestbookSeq;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public List<GuestbookRes> findGuestbookList(long roomSeq, long userSeq) {
-        return null;
+        log.debug("[POST] Service - findGuestbookList");
+        List<GuestbookRes> guestbookResList;
+        User user = userRepository.findById(userSeq).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
+        try {
+            guestbookResList = guestbookRepository.findAllByRoom_RoomSeq(roomSeq).stream().map(GuestbookRes::toDto).collect(Collectors.toList());
+            log.debug("guestbooks size: " + guestbookResList.size());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            guestbookResList = null;
+        }
+        return guestbookResList;
     }
 
     @Override
     public Boolean deleteGuestbook(long guestbookSeq, long userSeq) {
+        log.debug("[POST] Service - deleteGuestbook");
+        User user = userRepository.findById(userSeq).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
         return null;
     }
 
     @Override
     public Long insertGuestbookComment(GuestbookCommentReq req, long guestbookSeq, long userSeq) {
+        log.debug("[POST] Service - insertGuestbookComment");
+        User user = userRepository.findById(userSeq).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
         return null;
     }
 
     @Override
     public Boolean deleteGuestbookComment(long guestbookSeq, long userSeq) {
+        log.debug("[POST] Service - deleteGuestbookComment");
+        User user = userRepository.findById(userSeq).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
         return null;
     }
 }
