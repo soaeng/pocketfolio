1a7588e4723a041910c693bcc4cb9cd42a03566a
배상현 <counhealer@naver.com>
2022-11-09 22:56:04 +0900

    [Fix] 유저 번호 및 블로그 URL 조회

--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/db/repository/UserRepository.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/db/repository/UserRepository.java
@@ -12,16 +12,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
 
-	@Query(value = "select u.user_seq, u.email, u.name, ifnull(u.profile_pic, \"\") as profilePic, u.birth, " +
+	@Query(value = "select u.user_seq as userSeq, u.email, u.name, ifnull(u.profile_pic, \"\") as profilePic, u.birth, " +
 			"(select count(*) from follow where user_to = u.user_seq) as followerTotal, " +
 			"(select count(*) from follow where user_from = u.user_seq) as followingTotal, " +
-			"u.describe, u.blog_url from user as u where user_seq = ?1", nativeQuery = true)
+			"u.describe, u.blog_url as blogUrl from user as u where user_seq = ?1", nativeQuery = true)
 	Optional<UserView> findProfileById(long userSeq);
 
-	@Query(value = "select u.user_seq, u.email, u.name, ifnull(u.profile_pic, \"\") as profilePic, u.birth, " +
+	@Query(value = "select u.user_seq as userSeq, u.email, u.name, ifnull(u.profile_pic, \"\") as profilePic, u.birth, " +
 			"(select count(*) from follow where user_to = u.user_seq) as followerTotal, " +
 			"(select count(*) from follow where user_from = u.user_seq) as followingTotal, " +
-			"u.describe, u.blog_url from user as u", nativeQuery = true)
+			"u.describe, u.blog_url as blogUrl from user as u", nativeQuery = true)
 	List<UserView> findAllProfile();
 
 //	List<User> findByNameLikeOrderByCreated(String name);
