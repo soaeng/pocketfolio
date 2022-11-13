cb63b68167e5dcacd84b8868b397df8b7a75af4e
soaeng <sodud0695@naver.com>
2022-11-13 17:25:50 +0900

    [Add] 포트폴리오 이미지 등록 Swagger

--- backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/controller/PortfolioController.java
+++ backend/Pocketfolio/src/main/java/com/ssafy/pocketfolio/api/controller/PortfolioController.java
@@ -1,5 +1,6 @@
 package com.ssafy.pocketfolio.api.controller;
 
+import com.nimbusds.oauth2.sdk.ErrorResponse;
 import com.ssafy.pocketfolio.api.dto.PortfolioUrlDto;
 import com.ssafy.pocketfolio.api.dto.request.PortfolioReq;
 import com.ssafy.pocketfolio.api.dto.response.PortfolioListRes;
@@ -33,7 +34,7 @@ public class PortfolioController {
     @Operation(summary = "포트폴리오 등록", description = "포트폴리오 등록 시 포트폴리오 정보는 application/json 형식, thumbnail은 이미지 파일, files은 첨부파일(다중 첨부 가능)", responses = {
             @ApiResponse(responseCode = "201", description = "포트폴리오 등록 성공", content = @Content(schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
-            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping
     private ResponseEntity<Long> insertPortfolio(HttpServletRequest request, @RequestPart(value="portfolio") PortfolioReq portfolio, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail, @RequestPart(value = "files", required = false) List<MultipartFile> files) {
@@ -60,7 +61,7 @@ public class PortfolioController {
     @Operation(summary = "내 포트폴리오 목록", description = "내 포트폴리오 목록 조회", responses = {
             @ApiResponse(responseCode = "200", description = "포트폴리오 목록 조회 성공", content = @Content(schema = @Schema(implementation = PortfolioRes.class))),
             @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
-            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping
     private ResponseEntity<List<PortfolioListRes>> findPortfolioList(HttpServletRequest request){
@@ -87,7 +88,7 @@ public class PortfolioController {
     @Operation(summary = "포트폴리오 상세 조회", description = "포트폴리오 상세 조회", responses = {
             @ApiResponse(responseCode = "200", description = "포트폴리오 목록 조회 성공", content = @Content(schema = @Schema(implementation = PortfolioRes.class))),
             @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
-            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping("/{portSeq}")
     private ResponseEntity<PortfolioRes> findPortfolio(@PathVariable(value = "portSeq") long portSeq){
@@ -107,7 +108,7 @@ public class PortfolioController {
     @Operation(summary = "포트폴리오 수정", description = "포트폴리오 수정", responses = {
             @ApiResponse(responseCode = "201", description = "포트폴리오 수정 성공 후 수정된 포트폴리오 번호 반환", content = @Content(schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
-            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PatchMapping("/{portSeq}")
     private ResponseEntity<Long> updatePortfolio(@PathVariable(value="portSeq") long portSeq, @RequestPart(value="portfolio") PortfolioReq portfolio, @RequestPart(value="urls") List<PortfolioUrlDto> urls, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail, @RequestPart(value = "files", required = false) List<MultipartFile> files, HttpServletRequest request) {
@@ -136,7 +137,7 @@ public class PortfolioController {
     @Operation(summary = "포트폴리오 삭제", description = "포트폴리오 삭제", responses = {
             @ApiResponse(responseCode = "200", description = "포트폴리오 삭제 성공", content = @Content(schema = @Schema(implementation = Boolean.class))),
             @ApiResponse(responseCode = "403", description = "사용 불가능 토큰", content = @Content(schema = @Schema(implementation = UserRes.class))),
-            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = Error.class)))
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @DeleteMapping("/{portSeq}")
     private ResponseEntity<Boolean> deletePortfolio(@PathVariable(value="portSeq") long portSeq, HttpServletRequest request) {
@@ -161,6 +162,10 @@ public class PortfolioController {
         return new ResponseEntity<>(response, status);
     }
 
+    @Operation(summary = "포트폴리오 이미지 등록", description = "포트폴리오 이미지 등록", responses = {
+            @ApiResponse(responseCode = "200", description = "포트폴리오 이미지 등록 성공", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/images")
     private ResponseEntity<String> insertPortfolio(@RequestParam(value="image") MultipartFile image) {
         log.debug("[POST] Controller - insertPortfolio");
