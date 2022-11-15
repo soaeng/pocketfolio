b6c5a8ba4dce94e960cbdf0cfd0deb46361a2464
이승현 <window8397@naver.com>
2022-11-15 15:30:14 +0900

    [Feat] 최근 일주일 방문자 수

--- frontend/pocketfolio/src/components/room/HitDetail.jsx
+++ frontend/pocketfolio/src/components/room/HitDetail.jsx
@@ -1,15 +1,41 @@
-import {Container} from './HitDetail.style';
+import {
+  Container,
+  ChartContainer,
+  ListContainer,
+  DateContainer,
+  GraphBox,
+  KeyFrameBox,
+  Graph,
+  DateText,
+  ValueBox,
+  Value,
+} from './HitDetail.style';
 import {useDispatch} from 'react-redux';
 import {getVisitors} from '../../store/roomSlice';
 import {useEffect, useState} from 'react';
 
 const HitDetail = ({closeHit, data}) => {
   const dispatch = useDispatch();
-  const [hitData, setHitData] = useState(null);
+  const [visitors, setVisitors] = useState(null);
+  const [today, setToday] = useState(0);
+  const [hitStat, setHitStat] = useState([]);
+  const [max, setMax] = useState(0);
 
   async function getHitData() {
     const {payload} = await dispatch(getVisitors(data.room.roomSeq));
-    setHitData(payload);
+    setVisitors(payload.guests);
+    setToday(payload.today);
+
+    // 객체 데이터를 배열로
+    const value = Object.entries(payload.hitStat.hitStat).reverse();
+    setHitStat(value);
+
+    // 가장 많이 방문한 날짜
+    let map = new Map(value);
+    const maxVal = [...map.entries()].reduce((a, b) =>
+      a[1] > b[1] ? a : b,
+    )[1];
+    setMax(maxVal + 1);
   }
 
   useEffect(() => {
@@ -18,7 +44,23 @@ const HitDetail = ({closeHit, data}) => {
 
   return (
     <Container>
-      <>Hit</>
+      <ChartContainer>
+        {hitStat.map((item, idx) => (
+          <DateContainer>
+            <GraphBox>
+              <KeyFrameBox>
+                <Graph style={{height: `${(item[1] / max) * 100}%`}}>
+                  <ValueBox className={item[1] ? "" : "noShow"}>
+                    <Value>{`${item[1]}`}</Value>
+                  </ValueBox>
+                </Graph>
+              </KeyFrameBox>
+            </GraphBox>
+            <DateText>{`${item[0].slice(8)}`}</DateText>
+          </DateContainer>
+        ))}
+      </ChartContainer>
+      <ListContainer></ListContainer>
     </Container>
   );
 };
--- frontend/pocketfolio/src/components/room/HitDetail.style.js
+++ frontend/pocketfolio/src/components/room/HitDetail.style.js
@@ -4,4 +4,83 @@ export const Container = styled.div`
   margin-top: 0.6rem;
   padding: 1rem 0 0.5rem 0;
   width: 100%;
+
+  display: flex;
+  justify-content: space-between;
 `;
+
+export const ChartContainer = styled.div`
+  width: calc(60% - 1rem);
+  padding: 0.5rem;
+  aspect-ratio: 4/3;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const DateContainer = styled.div`
+  height: 100%;
+  width: 13%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const GraphBox = styled.div`
+  height: 80%;
+  display: flex;
+  flex-direction: column-reverse;
+`;
+
+export const KeyFrameBox = styled.div`
+  overflow: hidden;
+  animation: upTo 1s 0s 1 forwards;
+  display: flex;
+  flex-direction: column-reverse;
+
+  @keyframes upTo {
+    0% {
+      height: 0;
+    }
+    100% {
+      height: 100%;
+    }
+  }
+`;
+
+export const Graph = styled.div`
+  background-color: #e75452;
+  width: 0.7rem;
+  border-bottom: 2px solid #e75452;
+
+  position: relative;
+`;
+
+export const ValueBox = styled.div`
+  position: absolute;
+  top: -3rem;
+
+  &.noShow {
+    display: none;
+  }
+`;
+
+export const Value = styled.p`
+  font-weight: bold;
+`;
+
+
+
+export const DateText = styled.p`
+
+`
+
+export const ListContainer = styled.div`
+  background-color: yellow;
+  width: calc(40% - 1rem);
+  padding: 0.5rem;
+`;
+
