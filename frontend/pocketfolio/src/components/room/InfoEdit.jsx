6d222f5169ce043751aa56f15b682608ae79b8e4
이승현 <window8397@naver.com>
2022-11-16 23:44:40 +0900

    [Add] 포켓 정보 수정

--- frontend/pocketfolio/src/components/room/InfoEdit.jsx
+++ frontend/pocketfolio/src/components/room/InfoEdit.jsx
@@ -1,9 +1,128 @@
+import {useEffect, useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {getRoomCategory, updateRoom} from '../../store/roomSlice';
+import Modal from '../common/Modal';
+import {
+  Container,
+  CancelBox,
+  CancelIcon,
+  Title,
+  Div,
+  Name,
+  NameInput,
+  SelectBox,
+  Selected,
+  SelectOption,
+  IconDiv,
+  ShowIcon,
+  NoshowIcon,
+  TFBox,
+  TIcon,
+  FIcon,
+  Btn,
+} from './InfoEdit.style';
+
 const InfoEdit = ({closeInfoEdit, data}) => {
-  console.log(data)
+  const dispatch = useDispatch();
+  console.log(data);
+
+  const [categorys, setCategorys] = useState([]);
+  const [name, setName] = useState(data.room.name);
+  const [category, setCategory] = useState(data.room.category);
+  const [isMain, setIsMain] = useState(data.room.isMain === 'T' ? true : false);
+  const [privacy, setPrivacy] = useState(
+    data.room.privacy === 'O' ? true : false,
+  );
+
+  const [open, setOpen] = useState(false);
+
+  async function loadCategory() {
+    const {payload} = await dispatch(getRoomCategory());
+    setCategorys(payload);
+  }
+
+  async function sendData() {
+    const res = await dispatch(
+      updateRoom({
+        roomSeq: data.room.roomSeq,
+        data: {
+          room: {
+            name,
+            category: category.categorySeq,
+            theme: data.room.theme,
+            isMain: isMain ? 'T' : 'F',
+            privacy: privacy ? 'O' : 'C',
+          },
+        },
+      }),
+    );
+  }
+
+  useEffect(() => {
+    loadCategory();
+  }, []);
+
   return (
-    <>
-      <>InfoEdit</>
-    </>
+    <Modal>
+      <Container>
+        <CancelBox onClick={closeInfoEdit}>
+          <CancelIcon />
+        </CancelBox>
+        <Title>포켓 정보 수정</Title>
+        <Div>
+          <Name>포켓명</Name>
+          <NameInput
+            type="text"
+            value={name}
+            onChange={e => {
+              setName(e.target.value);
+            }}
+          />
+        </Div>
+
+        <Div className={open && 'open'}>
+          <Name>카테고리</Name>
+          <SelectBox className={open && 'open'}>
+            <Selected onClick={() => setOpen(!open)} className={open && 'open'}>
+              <SelectOption className="selected">{category.name}</SelectOption>
+              <IconDiv>{open ? <NoshowIcon /> : <ShowIcon />}</IconDiv>
+            </Selected>
+
+            {categorys.map(
+              (item, idx) =>
+                item.categorySeq !== category.categorySeq && (
+                  <SelectOption
+                    className={!open && 'close'}
+                    onClick={() => {
+                      setCategory(item);
+                      setOpen(!open);
+                    }}
+                  >
+                    {item.name}
+                  </SelectOption>
+                ),
+            )}
+          </SelectBox>
+        </Div>
+
+        <Div>
+          <Name>메인 설정</Name>
+          <TFBox onClick={() => setIsMain(!isMain)}>
+            {isMain ? <TIcon /> : <FIcon />}
+          </TFBox>
+        </Div>
+        <Div>
+          <Name>공개 설정</Name>
+          <TFBox onClick={() => setPrivacy(!privacy)}>
+            {privacy ? <TIcon /> : <FIcon />}
+          </TFBox>
+        </Div>
+
+        <Div>
+          <Btn onClick={sendData}>저장</Btn>
+        </Div>
+      </Container>
+    </Modal>
   );
 };
 
--- frontend/pocketfolio/src/components/room/InfoEdit.style.js
+++ frontend/pocketfolio/src/components/room/InfoEdit.style.js
@@ -1,1 +1,151 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import {
+  RiCloseFill,
+  RiArrowDownSFill,
+  RiArrowUpSFill,
+  RiCheckboxBlankLine,
+  RiCheckboxFill,
+} from 'react-icons/ri';
+
+export const Container = styled.div`
+  position: relative;
+  color: #333333;
+  width: 30rem;
+  /* height: 80vh; */
+
+  @media screen and (max-width: 35rem) {
+    width: 85vw;
+  }
+`;
+
+export const CancelBox = styled.div`
+  position: absolute;
+  top: -0.8rem;
+  right: 0;
+  width: 1.5rem;
+  aspect-ratio: 1/1;
+`;
+
+export const CancelIcon = styled(RiCloseFill)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Title = styled.h2`
+  margin-right: 2rem;
+  width: 100%;
+  text-align: center;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &.open {
+    align-items: flex-start;
+  }
+
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+export const Name = styled.h4`
+  margin: 0.6rem 0;
+  margin-right: 1rem;
+`;
+
+export const NameInput = styled.input`
+  padding: 0.6rem;
+  border: 2px solid #ffe4de;
+  border-radius: 0.3rem;
+  width: 15rem;
+  font-size: 1rem;
+
+  @media screen and (max-width: 35rem) {
+    width: 70%;
+  }
+
+  &:focus {
+    outline: none;
+    border-color: #f6b9ab;
+  }
+`;
+
+export const SelectBox = styled.div`
+  padding: 0.6rem;
+  border: 2px solid #ffe4de;
+  border-radius: 0.3rem;
+  position: relative;
+
+  &.open {
+    border-color: #f6b9ab;
+  }
+`;
+
+export const Selected = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &.open {
+    padding-bottom: 0.6rem;
+    border-bottom: 2px solid #f6b9ab;
+    margin-bottom: 0.6rem;
+  }
+`;
+
+export const SelectOption = styled.div`
+  display: block;
+  width: 9rem;
+
+  & + & {
+    padding-top: 0.6rem;
+  }
+
+  &.close {
+    display: none;
+  }
+
+  &:hover {
+    font-weight: bold;
+  }
+
+  &.selected {
+    font-weight: initial;
+  }
+`;
+
+export const IconDiv = styled.div`
+  font-size: 1.2rem;
+`;
+
+export const ShowIcon = styled(RiArrowDownSFill)``;
+export const NoshowIcon = styled(RiArrowUpSFill)``;
+
+export const TFBox = styled.div`
+  font-size: 1.4rem;
+  height: 1.4rem;
+`;
+
+export const TIcon = styled(RiCheckboxFill)`
+  color: #f6b9ab;
+`;
+export const FIcon = styled(RiCheckboxBlankLine)`
+  color: #f6b9ab;
+`;
+
+export const Btn = styled.div`
+  padding: 0.5rem 1rem;
+  margin-top: 1rem;
+  border-radius: 0.5rem;
+  background-color: #f6b9ab;
+  color: white;
+  font-weight: bold;
+
+  &:hover {
+    background-color: #da9d8f;
+    color: #e3e2e2;
+  }
+`;
