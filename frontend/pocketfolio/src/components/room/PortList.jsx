4230abe4b7aab25f434a7c28e9f3aa77ce1a3fea
강민재 <kmj5052@gmail.com>
2022-11-18 22:35:12 +0900

    [Add] 포트폴리오 비었을 때 상황

--- frontend/pocketfolio/src/components/room/PortList.jsx
+++ frontend/pocketfolio/src/components/room/PortList.jsx
@@ -1,14 +1,28 @@
 import PortItem from './PortItem';
-import {Container, Title} from './PortList.style';
+import {Container, Title, Box, ContentTitle} from './PortList.style';
+import {ContentBox} from './PortItem.style';
+import {useNavigate} from 'react-router-dom';
 
 const PortList = ({data, openPortDetail}) => {
+  const navigate = useNavigate();
+  const moveCreatePort = () => {
+    navigate('/port/create');
+  };
   return (
     <Container>
       <Title>포트폴리오 목록</Title>
-
       {data.map((item, idx) => (
         <PortItem item={item} key={idx} openPortDetail={openPortDetail} />
       ))}
+      {data?.length === 0 && (
+        <Box onClick={moveCreatePort}>
+          <ContentBox className={'full'}>
+            <ContentTitle>
+              포트폴리오가 없습니다. 이곳을 클릭해서 포트폴리오를 만드세요.
+            </ContentTitle>
+          </ContentBox>
+        </Box>
+      )}
     </Container>
   );
 };
